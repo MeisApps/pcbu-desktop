c37e1edb08e81dcba844d263e347ec5bd128c41b
Meis Apps <admin@meis-apps.com>
2025-02-08 14:33:43 +0100

    CI: Use arm64 Linux runner

--- pkg/build-desktop.sh
+++ pkg/build-desktop.sh
@@ -36,11 +36,7 @@ if [[ "$PLATFORM" == "win" ]]; then
   cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR -G "Visual Studio 17 2022" -A $VS_ARCH -DCMAKE_GENERATOR_PLATFORM=$VS_ARCH
   cmake --build . --target "pcbu_desktop" --config Release -- /maxcpucount:3
 else
-  if [[ "$DOCKER_BUILD" == "1" ]]; then
-    cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11
-  else
-    cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR
-  fi
+  cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR
   cmake --build . --target "pcbu_auth" --config Release -- -j3
   cmake --build . --target "pam_pcbiounlock" --config Release -- -j3
   cmake --build . --target "pcbu_desktop" --config Release -- -j3
@@ -87,16 +83,8 @@ elif [[ "$PLATFORM" == "linux" ]]; then
 
   export QML_SOURCES_PATHS=../../desktop/qml
   export EXTRA_QT_MODULES=svg;
-  if [[ "$ARCH" == "arm64" ]] && [[ "$DOCKER_BUILD" == "1" ]]; then
-    qemu-aarch64-static ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin checkrt --desktop-file ../linux/PCBioUnlock.desktop
-    wget "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage" && chmod +x ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
-    qemu-aarch64-static ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage --appdir appimage_dir
-    rm ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
-    qemu-aarch64-static ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin checkrt --output appimage --desktop-file ../linux/PCBioUnlock.desktop
-  else
-    wget "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage" && chmod +x ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
-    ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin qt --plugin checkrt --output appimage --desktop-file ../linux/PCBioUnlock.desktop
-  fi
+  wget "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage" && chmod +x ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
+  ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin qt --plugin checkrt --output appimage --desktop-file ../linux/PCBioUnlock.desktop
   mv PC_Bio_Unlock*.AppImage PCBioUnlock.AppImage
   chmod +x PCBioUnlock.AppImage
 elif [[ "$PLATFORM" == "mac" ]]; then
