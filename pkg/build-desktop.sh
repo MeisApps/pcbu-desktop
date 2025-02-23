cde409b9bb28198a1d50917dc3066ebf48ea87cd
Meis Apps <113235084+MeisApps@users.noreply.github.com>
2025-02-23 01:40:36 +0100

    Qt 6.8.2 (#43)
    * Qt 6.8.2
    
    * CI: Use arm64 Linux runner
    
    * Use Ubuntu 24.04 for Linux build
    
    * Workaround for linuxdeploy bug
    
    * Fix QT warning

--- pkg/build-desktop.sh
+++ pkg/build-desktop.sh
@@ -36,11 +36,7 @@ if [[ "$PLATFORM" == "win" ]]; then
   cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR -G "Visual Studio 17 2022" -A $VS_ARCH -DCMAKE_GENERATOR_PLATFORM=$VS_ARCH
   cmake --build . --target "pcbu_desktop" --config Release -- /maxcpucount:3
 else
-  if [[ "$DOCKER_BUILD" == "1" ]]; then
-    cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR -DCMAKE_C_COMPILER=gcc-11 -DCMAKE_CXX_COMPILER=g++-11
-  else
-    cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR
-  fi
+  cmake ../../ -DCMAKE_BUILD_TYPE=Release -DTARGET_ARCH=$ARCH -DQT_BASE_DIR=$QT_BASE_DIR
   cmake --build . --target "pcbu_auth" --config Release -- -j3
   cmake --build . --target "pam_pcbiounlock" --config Release -- -j3
   cmake --build . --target "pcbu_desktop" --config Release -- -j3
@@ -52,12 +48,12 @@ if [[ "$PLATFORM" == "win" ]]; then
   cp desktop/Release/pcbu_desktop.exe installer_dir/
   "/c/Program Files (x86)/Windows Kits/10/bin/10.0.22621.0/x64/mt.exe" -manifest ../win/requireAdmin.manifest -outputresource:installer_dir/pcbu_desktop.exe
 
-  "/c/Qt/6.7.2/msvc2019_64/bin/windeployqt" --qmldir ../../desktop/qml installer_dir/pcbu_desktop.exe
+  "/c/Qt/6.8.2/msvc2022_64/bin/windeployqt" --qmldir ../../desktop/qml installer_dir/pcbu_desktop.exe
   if [[ "$ARCH" == "arm64" ]]; then # ToDo: Workaround for no windeployqt on arm64
     find "installer_dir/" -type f -name "*.dll" | while read -r dll_file; do
       file_name=$(basename "$dll_file")
       if [[ "$file_name" != "D3Dcompiler_47.dll" ]] && [[ "$file_name" != "opengl32sw.dll" ]]; then
-        replacement_file=$(find "/c/Qt/6.7.2/msvc2019_arm64/" -type f -name "$file_name" | head -n 1)
+        replacement_file=$(find "/c/Qt/6.8.2/msvc2022_arm64/" -type f -name "$file_name" | head -n 1)
         if [ -f "$replacement_file" ]; then
           echo "Replacing $dll_file with $replacement_file"
           cp "$replacement_file" "$dll_file"
@@ -86,17 +82,12 @@ elif [[ "$PLATFORM" == "linux" ]]; then
   chmod +x appimage_dir/usr/bin/run-app.sh
 
   export QML_SOURCES_PATHS=../../desktop/qml
-  export EXTRA_QT_MODULES=svg;
-  if [[ "$ARCH" == "arm64" ]] && [[ "$DOCKER_BUILD" == "1" ]]; then
-    qemu-aarch64-static ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin checkrt --desktop-file ../linux/PCBioUnlock.desktop
-    wget "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage" && chmod +x ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
-    qemu-aarch64-static ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage --appdir appimage_dir
-    rm ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
-    qemu-aarch64-static ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin checkrt --output appimage --desktop-file ../linux/PCBioUnlock.desktop
-  else
-    wget "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage" && chmod +x ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
-    ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin qt --plugin checkrt --output appimage --desktop-file ../linux/PCBioUnlock.desktop
-  fi
+  export EXTRA_QT_MODULES=svg
+  ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin checkrt --desktop-file ../linux/PCBioUnlock.desktop
+  wget "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage" && chmod +x ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
+  ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage --appdir appimage_dir
+  rm ./linuxdeploy-plugin-qt-$LINUX_ARCH.AppImage
+  ./linuxdeploy-$LINUX_ARCH.AppImage --appdir appimage_dir --plugin checkrt --output appimage --desktop-file ../linux/PCBioUnlock.desktop
   mv PC_Bio_Unlock*.AppImage PCBioUnlock.AppImage
   chmod +x PCBioUnlock.AppImage
 elif [[ "$PLATFORM" == "mac" ]]; then
@@ -112,8 +103,10 @@ elif [[ "$PLATFORM" == "mac" ]]; then
   ln -s /Applications dmg_dir/Applications
 
   if [[ "$CI_BUILD" == "1" ]]; then
-    echo "Killing XProtect..."; sudo pkill -9 XProtect >/dev/null || true;
-    echo "Waiting for XProtect..."; while pgrep XProtect; do sleep 3; done;
+    echo "Killing XProtect..."
+    sudo pkill -9 XProtect >/dev/null || true
+    echo "Waiting for XProtect..."
+    while pgrep XProtect; do sleep 3; done
   fi
   hdiutil create -volname "PC Bio Unlock" -srcfolder dmg_dir/ -ov -format UDZO ./PCBioUnlock-$ARCH.dmg
 fi
