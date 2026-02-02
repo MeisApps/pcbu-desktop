0dfcefc197d847b355108dbd0457636ff1c40660
Meis Apps <113235084+MeisApps@users.noreply.github.com>
2026-02-02 04:15:09 +0100

    CI: Use Qt version from workflow

--- pkg/linux/docker-build.sh
+++ pkg/linux/docker-build.sh
@@ -6,13 +6,13 @@ cd ..
 
 if [[ "$ARCH" == "x64" ]]; then
   docker container prune -f
-  docker build -t pcbu_builder -f pkg/linux/Dockerfile_x64 .
+  docker build -t pcbu_builder -f pkg/linux/Dockerfile_x64 --build-arg QT="${QT_VERSION}" .
   docker run --name pcbu_build_container --cap-add=SYS_ADMIN --security-opt apparmor:unconfined --device /dev/fuse pcbu_builder
   docker cp pcbu_build_container:/project/pkg/build/PCBioUnlock.AppImage ./pkg/build/PCBioUnlock-x64.AppImage
   docker container rm pcbu_build_container
 elif [[ "$ARCH" == "arm64" ]]; then
   docker container prune -f
-  docker build -t pcbu_builder -f pkg/linux/Dockerfile_arm64 .
+  docker build -t pcbu_builder -f pkg/linux/Dockerfile_arm64 --build-arg QT="${QT_VERSION}" .
   docker run --name pcbu_build_container --cap-add=SYS_ADMIN --security-opt apparmor:unconfined --device /dev/fuse pcbu_builder
   docker cp pcbu_build_container:/project/pkg/build/PCBioUnlock.AppImage ./pkg/build/PCBioUnlock-arm64.AppImage
   docker container rm pcbu_build_container
