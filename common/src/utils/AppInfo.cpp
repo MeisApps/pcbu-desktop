499799d22749b722a48eb6d901ebee3775905f7c
Meis Apps <113235084+MeisApps@users.noreply.github.com>
2026-02-14 02:17:29 +0100

    Bump version

--- common/src/utils/AppInfo.cpp
+++ common/src/utils/AppInfo.cpp
@@ -6,7 +6,7 @@
 #include <spdlog/spdlog.h>
 
 std::string AppInfo::GetVersion() {
-  std::string version = "3.0.0";
+  std::string version = "3.0.1";
   if(PCBU_DEBUG)
     version += fmt::format("-{}-{}", GIT_BRANCH, GIT_COMMIT_HASH);
   return version;
