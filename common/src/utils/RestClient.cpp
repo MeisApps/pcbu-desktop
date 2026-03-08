247559655b05d351a4a7f3d0d4b2f473af677fdc
Meis Apps <admin@meis-apps.com>
2026-03-08 17:08:52 +0100

    Update version check URL

--- common/src/utils/RestClient.cpp
+++ common/src/utils/RestClient.cpp
@@ -5,12 +5,14 @@
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 
+#include "AppInfo.h"
+
 std::string RestClient::CheckForUpdates(const std::string &product, const std::string &platform) {
-  httplib::Client client("https://api.meis-apps.com");
+  httplib::Client client("https://meis-apps.com");
   client.set_connection_timeout(5, 0);
   client.set_read_timeout(5, 0);
   client.set_write_timeout(5, 0);
-  auto result = client.Get(fmt::format("/rest/appUpdater/checkUpdates?product={}&platform={}", product, platform));
+  auto result = client.Get(fmt::format("/rest/checkUpdates?product={}&platform={}&currVersion={}", product, platform, AppInfo::GetVersion()));
   if(!result)
     throw std::runtime_error(to_string(result.error()));
   auto resultJson = nlohmann::json::parse(result->body);
